217f1263267a8ec45cc0af954aebcfd39fb30eac
MannaICT13 <khmannaict13@gmail.com>
2021-03-21 14:49:51 +0600

    Initial Commit

---
+++ MyChatApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyChatApplication
+//
+//  Created by Md Khaled Hasan Manna on 21/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
